d695acb3f681cc145ee8bbd61512dc859b8612ff
Matteo Baccan <matteo.baccan@gmail.com>
2019-04-17 03:01:14 +0200

    Removed import

--- src/main/java/it/baccan/sockredirector/SockThread.java
+++ src/main/java/it/baccan/sockredirector/SockThread.java
@@ -11,7 +11,6 @@ package it.baccan.sockredirector;
 import it.baccan.sockredirector.pojo.ServerPojo;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.RandomAccessFile;
